2649b743e23f0df2088f60f859f0a66c06475fbc
Oleksandr <gayrsuk@gmail.com>
2024-03-24 20:08:03 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,12 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import { convertMs } from './helpers';
 
 const startButton = document.querySelector('button[data-start]');
+const datePickerField = document.querySelector('input#datetime-picker');
 const daysField = document.querySelector('span[data-days]');
 const hoursField = document.querySelector('span[data-hours]');
 const minutesField = document.querySelector('span[data-minutes]');
@@ -34,6 +39,7 @@ const startTimer = () => {
 startButton.addEventListener('click', () => {
   startTimer();
   startButton.disabled = true;
+  datePickerField.disabled = true;
 });
 
 const options = {
@@ -45,29 +51,20 @@ const options = {
     selectedDate = selectedDates[0].getTime();
     let difDate = selectedDate - dateNow.getTime();
     if (difDate <= 0) {
-      alert('Please choose a date in the future');
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        color: '#FF2E2E',
+        messageColor: '#ffffff',
+        backgroundColor: '#FF2E2E',
+        timeout: 5000,
+        close: false,
+      });
       return;
     }
+
+    startButton.disabled = false;
   },
 };
 
 flatpickr('input#datetime-picker', options);
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
